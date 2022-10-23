8c9eb812702f1ccfd2736742c062ea92de014c41
Noodkhan_404 <92358053+noodkhan@users.noreply.github.com>
2022-10-23 21:26:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h2>hello world</h2>
+          <a>คนที่ไม่มีความผิด คือคนที่มันไม่ทำอะไร</a>
+          <h3>คอยดู</h3>
+        </header>
+        <main className="Main">
+          <div></div>
+        </main>
+        <footer className="Feet">
+          <div></div>
+        </footer>
+      </div>
+    </>
   );
 }
 
